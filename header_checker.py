669ada1997078151a1447b29a519c003c8697211
Pintu Kumar Sutradhar <126931192+pintukumar-sutradhar@users.noreply.github.com>
2023-03-25 14:54:23 +0600

    Update header_checker.py

--- header_checker.py
+++ header_checker.py
@@ -1,32 +1,37 @@
 import requests
 
 # Define the URL of the web application to check
-url = input("Enter URL:")
+url = input("Enter URL: ")
+if not url.startswith("http://") and not url.startswith("https://"):
+    url = "http://" + url
 
-# Send a GET request to the URL to retrieve the headers
-response = requests.head(url)
+# Send a GET request to the URL to retrieve the page content
+response = requests.get(url)
 
-# Get the headers from the response object
-headers = response.headers
+# Print the status code of the response
+print(f"Status Code: {response.status_code}\n")
 
-# Check the headers for security-related settings
-security_headers = {
-    'X-XSS-Protection': '1; mode=block',
-    'X-Content-Type-Options': 'nosniff',
-    'Strict-Transport-Security': 'max-age=31536000; includeSubDomains; preload',
-    'Content-Security-Policy': "default-src 'self'",
-    'X-Frame-Options': 'DENY',
-}
+# Print the headers for the request and response
+print(f"Request Headers:")
+for header, value in response.request.headers.items():
+    print(f"{header}: {value}")
+print(f"\nResponse Headers:")
+for header, value in response.headers.items():
+    print(f"{header}: {value}")
 
+# Define the list of required headers and check if they are present in the response
+required_headers = ["Content-Type", "User-Agent", "Strict-Transport-Security", "X-Frame-Options",
+                    "X-XSS-Protection", "X-Content-Type-Options", "Referrer-Policy", "Content-Security-Policy",
+                    "Permissions-Policy"]
 missing_headers = []
-for header, value in security_headers.items():
-    if header not in headers:
+for header in required_headers:
+    if header not in response.headers:
         missing_headers.append(header)
 
+# Print the list of missing headers or a message indicating that all headers are present
 if missing_headers:
-    print(f"The following security headers are missing from {url}:")
+    print(f"\nMissing Headers:")
     for header in missing_headers:
-        print(f"- {header}")
+        print(header)
 else:
-    print(f"All security headers are present on {url}.")
-
+    print(f"\nAll required headers are present.")
